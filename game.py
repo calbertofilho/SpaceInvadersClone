a8da23ba32679c2ae7ded9074724c383d472b5e9
calbertofilho <84130607+calbertofilho@users.noreply.github.com>
2021-05-17 17:21:56 -0300

    Alterações 17/05/2021

--- game.py
+++ game.py
@@ -19,13 +19,13 @@ from random import randint, choice
 from sys import platform as plat, exit as ext, exc_info as info
 import pickle
 import pygame
-from pygame.constants import ( QUIT, KEYDOWN, K_ESCAPE, K_SPACE )
+from pygame.constants import ( K_LEFT, K_RIGHT, QUIT, KEYDOWN, K_ESCAPE, K_SPACE )
+import time
 # Constantes
 BASE_DIR = path.dirname(__file__)    # Diretorio do jogo
 SCREEN_WIDTH = 600                   # Comprimento da tela
 SCREEN_HEIGHT = 860                  # Altura da tela
-GRAVITY = 1                          # Gravidade
-GAME_SPEED = SPEED = 10              # Velocidades
+SPEED = 10                           # Velocidades
 FPS = 30                             # Frames por segundo
 
 class Ship(pygame.sprite.Sprite):
@@ -33,38 +33,55 @@ class Ship(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.images = [
-            pygame.image.load(
-                f'{BASE_DIR}/assets/sprites/player/player.png'
-            ).convert_alpha(),
-            pygame.image.load(
-                f'{BASE_DIR}/assets/sprites/player/player_.png'
-            ).convert_alpha()
+            pygame.image.load(f'{BASE_DIR}/assets/sprites/player/player.png').convert_alpha(),
+            pygame.image.load(f'{BASE_DIR}/assets/sprites/player/player_.png').convert_alpha()
         ]
-        self.speed = SPEED
         self.current_image = 0
         self.image = self.images[self.current_image]
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect[0] = (SCREEN_WIDTH / 2) - (self.image.get_width() / 2)
-        self.rect[1] = SCREEN_HEIGHT / 2
+        self.rect[1] = SCREEN_HEIGHT - self.image.get_height()
 
     def update(self):
-        '''Função que representa como o pássaro se comporta em cada interação no jogo'''
-        self.speed += GRAVITY
-        self.current_image = (self.current_image + 1) % 3
-        self.image = pygame.transform.rotozoom(self.images[self.current_image], self.speed * -3, 1)
-        self.rect[1] += self.speed
+        '''Função que representa como a nave se comporta em cada interação no jogo'''
+        self.current_image = (self.current_image + 1) % len(self.images)
+        self.image = self.images[self.current_image]
 
-    def shoot(self):
-        '''Função que representa a ação de voar do pássaro'''
-        self.speed = -SPEED
+    def shoot(self, sound):
+        '''Função que representa a ação de atirar com a nave'''
+        pygame.mixer.Sound.play(sound)
+        return Laser(self.rect[0] + 33, self.rect[1] - 28)
 
     def get_width(self):
-        '''Função que retorna o comprimento do pássaro'''
+        '''Função que retorna a largura da nave'''
         return self.image.get_size()[0]
 
     def get_height(self):
-        '''Função que retorna a altura do pássaro'''
+        '''Função que retorna o comprimento da nave'''
+        return self.image.get_size()[1]
+
+class Laser(pygame.sprite.Sprite):
+    '''Classe que representa o laser da nave'''
+    def __init__(self, pos_x, pos_y):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(f'{BASE_DIR}/assets/sprites/player/bullet.png').convert_alpha()
+        self.mask = pygame.mask.from_surface(self.image)
+        self.rect = self.image.get_rect()
+        self.rect[0] = pos_x
+        self.rect[1] = pos_y
+        self.speed = SPEED
+
+    def update(self):
+        '''Função que representa a trajetória do laser'''
+        self.rect[1] -= self.speed
+
+    def get_width(self):
+        '''Função que retorna a largura do laser'''
+        return self.image.get_size()[0]
+
+    def get_height(self):
+        '''Função que retorna o comprimento do laser'''
         return self.image.get_size()[1]
 
 
@@ -91,9 +108,16 @@ def main():
         pygame.mixer.Sound(f'{BASE_DIR}/assets/sounds/{sound_type}/explosion.{sound_type}'),
         pygame.mixer.Sound(f'{BASE_DIR}/assets/sounds/{sound_type}/shot.{sound_type}')
     ]
+    # Criação das mensagens do jogo
+    messages = [
+        pygame.image.load(f'{BASE_DIR}/assets/sprites/messages/start_game.png').convert_alpha()
+#        pygame.image.load(f'{BASE_DIR}/assets/sprites/messages/get_ready.png').convert_alpha()
+#        pygame.image.load(f'{BASE_DIR}/assets/sprites/messages/game_over.png').convert_alpha()
+#        pygame.image.load(f'{BASE_DIR}/assets/sprites/messages/high_score.png').convert_alpha()
+    ]
     # Criação da imagem de fundo
     backgrounds = [
-        pygame.image.load(f'{BASE_DIR}/assets/sprites/sceneries/bg0.png'),
+        pygame.image.load(f'{BASE_DIR}/assets/sprites/sceneries/bg0_title-screen.png'),
         pygame.image.load(f'{BASE_DIR}/assets/sprites/sceneries/bg1.png'),
         pygame.image.load(f'{BASE_DIR}/assets/sprites/sceneries/bg2.png'),
         pygame.image.load(f'{BASE_DIR}/assets/sprites/sceneries/bg3.png'),
@@ -102,20 +126,27 @@ def main():
         pygame.image.load(f'{BASE_DIR}/assets/sprites/sceneries/bg6.png'),
         pygame.image.load(f'{BASE_DIR}/assets/sprites/sceneries/bg7.png'),
         pygame.image.load(f'{BASE_DIR}/assets/sprites/sceneries/bg8.png'),
-        pygame.image.load(f'{BASE_DIR}/assets/sprites/sceneries/bg9.png')
+        pygame.image.load(f'{BASE_DIR}/assets/sprites/sceneries/bg9.png'),
+        pygame.image.load(f'{BASE_DIR}/assets/sprites/sceneries/bg10_big-boss.png')
     ]
     background = backgrounds[0]
     background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
+    ship_group = pygame.sprite.Group()
+    ship = Ship()
+    ship_group.add(ship)
+    laser_group = pygame.sprite.Group()
     # Criação do controle de tempo do jogo
     clock = pygame.time.Clock()
     # Laço da tela de abertura do jogo
+    ship.rect[1] -= 400
     while splash:
-#        splash = messages[0]
-#        splash_x = SCREEN_WIDTH / 2 - splash.get_width() / 2
-#        splash_y = SCREEN_HEIGHT / 2 - splash.get_height() / 2
+        # Controle da velocidade do jogo
+        clock.tick(FPS)
+        splash = messages[0]
+        splash_x = 0
+        splash_y = 0
         screen.blit(background, (0, 0))
-#        screen.blit(splash, (splash_x, splash_y))
-        pygame.display.update()
+        screen.blit(splash, (splash_x, splash_y))
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
@@ -127,7 +158,16 @@ def main():
                 if event.key == K_SPACE:
                     splash = False
                     run = True
+        ship_group.update()
+        ship_group.draw(screen)
+        pygame.display.update()
     level = 1
+    screen_limit_left = 0
+    screen_limit_right = ((SCREEN_WIDTH - ship.get_width()) // 10) * 10
+    screen_limit_bottom = ((SCREEN_HEIGHT - ship.get_height()) // 10) * 10
+    start_time = [ time.localtime()[3], time.localtime()[4], time.localtime()[5] ]
+    elapsed_time = [ start_time[0] - time.localtime()[3], start_time[1] - time.localtime()[4], start_time[2] - time.localtime()[5] ]
+    print(elapsed_time)
     while run:
         # Controle da velocidade do jogo
         clock.tick(FPS)
@@ -141,15 +181,28 @@ def main():
             if event.type == KEYDOWN:
                 # Teste para saber se a tecla é "BARRA DE ESPAÇO"
                 if event.key == K_SPACE:
-                    #ship.shoot()
-                    pygame.mixer.Sound.play(sounds[2])
+                    laser_group.add(ship.shoot(sounds[2]))
+
+
+        commands = pygame.key.get_pressed()
+        # Teste para saber se a tecla é "SETA PARA A ESQUERDA"
+        if commands[K_LEFT]:
+            ship.rect[0] -= SPEED if ((ship.rect[0] // 10) * 10) != screen_limit_left else 0
+        # Teste para saber se a tecla é "SETA PARA A DIREITA"
+        if commands[K_RIGHT]:
+            ship.rect[0] += SPEED if ((ship.rect[0] // 10) * 10) != screen_limit_right else 0
+
         background = backgrounds[level]
         background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
         # Desenho da imagem de fundo do jogo
         screen.blit(background, (0, 0))
+        ship_group.update()
+        laser_group.update()
+        ship_group.draw(screen)
+        laser_group.draw(screen)
+        ship.rect[1] += SPEED if ((ship.rect[1] // 10) * 10) != screen_limit_bottom else 0
 
         pygame.display.update()
-
 
 try:
     while True:
